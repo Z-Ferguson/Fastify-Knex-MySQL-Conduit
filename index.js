cddd1939e16845b5b49b03ef6c67b2fa7243d98a
Zach_Personal <Zfergusonn@gmail.com>
2023-05-11 22:14:28 -0400

    Fastify Configs Playing with settings and getting familiar with fastify

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-// const getConfig = require("./lib/config/config");
-// const startServer = require("./lib/server");
+const getConfig = require("./src/config/index");
+// const startServer = require("./src/server");
+const startServer = require("./src/app");
 
-// const main = async () => {
-//   process.on("unhandledRejection", (err) => {
-//     console.error(err);
-//     process.exit(1);
-//   });
-//   const config = await getConfig();
+const main = async () => {
+  process.on("unhandledRejection", (err) => {
+    console.error(err);
+    process.exit(1);
+  });
+  const config = await getConfig();
+  console.log(config);
 
-//   const server = require("fastify")(config.fastifyInit);
-//   server.register(startServer, config);
+  const server = require("fastify")(config.fastifyInit);
+  server.register(startServer, config);
 
-//   const address = await server.listen(config.fastify);
-//   server.log.info(`Server running at: ${address}`);
-// };
+  const address = await server.listen(config.fastify);
+  server.log.info(`Server running at: ${address}`);
+};
 
-// main();
+main();
